1e3faa52774fc4e47c7dd638c8de1224757bfc09
bsy7616 <bsy7616@gmail.com>
2026-03-16 16:55:35 +0000

    Fix: Pass FIREBASE_WEBAPP_CONFIG to the client

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_FIREBASE_WEBAPP_CONFIG: process.env.FIREBASE_WEBAPP_CONFIG,
+  },
 };
 
 export default nextConfig;
